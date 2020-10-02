dac35db889969c9eaa830affc3ae84fedd8dd194
misterdepth <62363912+misterdepth@users.noreply.github.com>
2020-10-02 07:57:46 -0700

    Fixed Things + MiniBoss Alerts
    Added MiniBoss Alerts + Crediting, and fixed the window slightly.

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ import { Setting, SettingsObject } from "../SettingsManager/SettingsManager";
 
 class Settings {
     constructor() {
-        settings.setCommand("dungeon").setSize(624, 278);
+        settings.setCommand("dungeon").setSize(550, 200);
         Setting.register(settings);
     }
 }
@@ -14,7 +14,7 @@ const settings = new SettingsObject(
         {
             name: "Main",
             settings: [
-                new Setting.Button("                                        &eDungeonUtilities", "&dBETA v0.9.99", () => {}),
+                new Setting.Button("                                        &eDungeonUtilities", "&dBETA v0.9.9", () => {}),
                 new Setting.Button("&bA collection of useful utilities for dungeons!", "By &aAntonio32A", () => {}),
                 new Setting.Button("", "", () => {}),
                 new Setting.Button("                                        &dCredits", "", () => {}),
@@ -23,8 +23,7 @@ const settings = new SettingsObject(
                 new Setting.Button("&dVals", "Map rendering fixes and the riddle solver.", () => {}),
                 new Setting.Button("&6DJtheRedstoner", "Map scale and the map background feature.", () => {}),
                 new Setting.Button("&cDebuggings", "Help with the Better Glowing Effect feature.", () => {}),
-                new Setting.Button("&3SirDesco", "Trivia Room Solver.", () => {}),
-                new Setting.Button("&2El_Oxo", "Help with the GitHub management.", () => {}),
+                new Setting.Button("&amisterdepth", "MiniBoss Alerts", () => {}),
                 new Setting.Button("", "", () => {}),
                 new Setting.Button("&dNote: &eAll mods are &cuse at your own risk&e.", "&cWARNING", () => {
                     ChatLib.chat(
@@ -38,16 +37,7 @@ const settings = new SettingsObject(
                         "&cIf you ever decide to uninstall the mod, do /ct delete DungeonUtilities"
                     );
                 }),
-                new Setting.Button("", "", () => {}),
-                new Setting.Button("&eDiscord Support Server:", "&9https://discord.gg/knjSZSC", () => {
-                    java.awt.Desktop.getDesktop().browse(new java.net.URI("https://discord.gg/knjSZSC"));
-                }),
-                new Setting.Button("&ePatreon:", "&9https://www.patreon.com/dungeonutilities", () => {
-                    java.awt.Desktop.getDesktop().browse(new java.net.URI("https://www.patreon.com/dungeonutilities"));
-                }),
-                new Setting.Button("&eGitHub:", "&9https://github.com/Antonio32A/DungeonUtilities", () => {
-                    java.awt.Desktop.getDesktop().browse(new java.net.URI("https://github.com/Antonio32A/DungeonUtilities"));
-                })
+                new Setting.Button("&eDiscord Support Server: &9https://discord.gg/knjSZSC", "", () => {})
             ]
         },
         {
@@ -112,12 +102,12 @@ const settings = new SettingsObject(
                 new Setting.Slider("X", 100, 0, Renderer.screen.getWidth(), 0),
                 new Setting.Slider("Y", 200, 0, Renderer.screen.getHeight(), 0)
             ]
-        },
+        }, 
         {
-            name: "Trivia Solver",
+            name: "MiniBoss Alerts",
             settings: [
-                new Setting.Button("", "Solves the trivia.", () => {}),
-                new Setting.Toggle("Enabled", false)
+                new Setting.Button("", "&eAlerts you when a MiniBoss is near.", () => {}),
+                new Setting.Toggle("Enabled", false),
             ]
         }
     ]
