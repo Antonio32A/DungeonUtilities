f2f92f8d6b29773df691279a0062b9b524fa1d29
misterdepth <62363912+misterdepth@users.noreply.github.com>
2020-10-02 07:55:37 -0700

    Fixed Things
    If the settings don't work in loader.js lemme know.

--- features/miniAlert.js
+++ features/miniAlert.js
@@ -1,25 +1,36 @@
-//Thanks Vals
-function inDungeons() {
-    if(!(Scoreboard.getTitle().trim() != "" && Scoreboard.getLines().length > 0)) {
+const inDungeons = () => {
+    if (!(Scoreboard.getTitle().trim() != "" && Scoreboard.getLines().length > 0)) {
         return;
     }
-    for(let i = 0; i < Scoreboard.getLines().length; i++) {
-        let line = Scoreboard.getLines()[i]
-        if(line.getName().includes('§cThe Cata') && line.getName().includes('mbs §8('))
-            return true
-    }
-    return false
+    Scoreboard.getLines(false).forEach(line => {
+        if (line.getName().includes('§cThe Cata') && line.getName().includes('mbs §8('));
+        return true;
+    })
+    return false;
 }
-register("renderWorld", () => {
-    if(settings.getSetting("Shit", "MoreShit") && inDungeons()) {
-        World.getAllEntities().forEach(e => {
-            if(e.getName().includes("Lost Adventurer")) Client.showTitle("", "&dLost Adventurer", 0, 3, 0)
-            if(e.getName().includes("Angry Archeologist")) Client.showTitle("", "&cAngry Archeologist", 0, 3, 0)
-            if(e.getName().includes("Shadow Assassin")) Client.showTitle("", "&5Shadow Assassin", 0, 3, 0)
-            if(e.getName().includes("King Midas")) Client.showTitle("", "&6King Midas", 0, 3, 0)
-        }) 
-    }
-})
 
-//Thsi code is amazing and everoyne iwll like it
-//- misterdepth
+class miniAlert {
+    constructor() {
+        this.name = "MiniBoss Alerts"
+        this.triggers = {
+            "renderWorld": [{
+                "func": this.renderWorld,
+                "criteria": null
+            }],
+        }
+
+    }
+            
+    renderWorld() {
+        if (inDungeons) {
+            World.getAllEntities().forEach(e => {
+                if (e.getName().includes("Lost Adventurer")) Client.showTitle("", "&dLost Adventurer", 0, 5, 0);
+                if (e.getName().includes("Angry Archeologist")) Client.showTitle("", "&cAngry Archeologist", 0, 5, 0);
+                if (e.getName().includes("Shadow Assassin")) Client.showTitle("", "&5Shadow Assassin", 0, 5, 0);
+                if (e.getName().includes("King Midas")) Client.showTitle("", "&6King Midas", 0, 5, 0);
+            });
+        }
+    }
+}
+    
+module.exports = { Feature: miniAlert }
