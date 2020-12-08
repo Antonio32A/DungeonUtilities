8c45b7c99fd5f59cd3e4b3f31f54afc97396ad25
Dolphin0xyz <63311818+Dolphin0xyz@users.noreply.github.com>
2020-12-08 21:39:58 +0000

    trivia year
    when trivia question is skyblock year it now tells you the year

--- features/trivia.js
+++ features/trivia.js
@@ -12,10 +12,24 @@ class Trivia {
         trivia.forEach(element => {
             register("chat", () => {
                 if (!settings.getSetting("Trivia", "Enabled")) return;
-                ChatLib.chat(`&eDungeonUtilities &8> &6 ` + element.answer);
+                if (element.answer === "Check the calendar for the answer.") {
+                    ChatLib.chat(`&eDungeonUtilities &8> &6 Year ` + sbYear());
+                } else {
+                    ChatLib.chat(`&eDungeonUtilities &8> &6 ` + element.answer);
+                }
             }).setCriteria(element.statements).setParameter("contains");
         });
     }
+}
+
+const Year1 = 1560718500000;
+const SByear = 446400000;
+
+function sbYear() {
+  let Time = new Date().getTime();
+  let Years = Time-Year1;
+  let YearNow = ((Years-(Years%SByear))/SByear)+2;
+  return YearNow;
 }
 
 module.exports = { Feature: Trivia }
